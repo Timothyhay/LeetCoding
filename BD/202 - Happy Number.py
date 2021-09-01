efd5b423a6d11d08d0904a8ae711bcc4815b7d81
RazzorLeaf <rogerforrio@gmail.com>
2021-09-01 15:23:51 +0800

    Organize file dir

---
+++ BD/202 - Happy Number.py
@@ -0,0 +1,19 @@
+# BD - 202
+
+from typing import List
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            sum = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                sum += digit ** 2
+            n = sum
+
+        return n == 1
+
