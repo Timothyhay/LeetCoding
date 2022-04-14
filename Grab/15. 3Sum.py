e6056671c138ca3f36a6de6b6a96a72185854092
RazzorLeaf <rogerforrio@gmail.com>
2022-04-14 17:40:10 +0800

    Array 3

---
+++ Grab/15. 3Sum.py
@@ -0,0 +1,31 @@
+'''
+    Array
+    Fix 1 value then do as 2Sum. 
+
+    2sum:
+    def twoSum(self, nums, target):
+        buffer_dictionary = {}
+        for i in rangenums.__len()):
+            if nums[i] in buffer_dictionary:
+                return [buffer_dictionary[nums[i]], i] #if a number shows up in the dictionary already that means the 
+                                                        #necesarry pair has been iterated on previously
+            else: # else is entirely optional
+                buffer_dictionary[target - nums[i]] = i 
+                # we insert the required number to pair with should it exist later in the list of numbers
+'''
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for fid in range(len(nums)-2):
+            a = nums[fid]
+            diff = dict()
+            for i in range(fid+1, len(nums)):
+                if nums[i] in diff:
+                    if [a, diff[nums[i]], nums[i]] not in ans:
+                        ans.append([a, diff[nums[i]], nums[i]])
+                else:
+                    diff[-a-nums[i]] = nums[i]
+        
+        return ans
