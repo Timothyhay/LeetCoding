f85fea32aeaba9f24b65308b876eeab6d5bfed3e
RazzorLeaf <rogerforrio@gmail.com>
2022-04-17 05:22:13 +0800

    Question

---
+++ Grab/Count number of triplets with product equal to given number.py
@@ -0,0 +1,17 @@
+'''
+    Array
+
+'''
+'''
+https://www.geeksforgeeks.org/count-number-triplets-product-equal-given-number/
+
+    Input : arr[] = { 1, 4, 6, 2, 3, 8}
+                m = 24
+    Output : 3
+    {1, 4, 6} {1, 3, 8} {4, 2, 3}
+    
+    Input : arr[] = { 0, 4, 6, 2, 3, 8}
+                m = 18
+    Output : 0
+'''
+
