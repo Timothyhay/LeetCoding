c92850a28ef0d39871ab41ba136fb693e1f707ec
RazzorLeaf <rogerforrio@gmail.com>
2022-05-02 18:45:06 +0800

    Matrix

--- Garena/RotateMatrix.py
+++ Garena/RotateMatrix.py
@@ -10,11 +10,11 @@ def rotate90Clockwise(arr) :
         for i in range(N - 1, -1, -1) :
             print(arr[i][j], end = " ")
         print()
-         
+
 # Driver code       
 arr = [ [ 1, 2, 3, 4 ],
           [ 5, 6, 7, 8 ],
           [ 9, 10, 11, 12 ],
           [ 13, 14, 15, 16 ] ]
-rotate90Clockwise(arr);
+rotate90Clockwise(arr)
  
