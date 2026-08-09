4c8a81194e47fc1feacf813976d7b779c2a68f1f
RazzorLeaf <rogerforrio@gmail.com>
2026-08-09 17:19:42 +0800

    JOIN

---
+++ SQL/1378. Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,8 @@
+--INNER JOIN：如果表中有至少一个匹配，则返回行
+--LEFT JOIN：即使右表中没有匹配，也从左表返回所有的行
+--RIGHT JOIN：即使左表中没有匹配，也从右表返回所有的行
+--FULL JOIN：只要其中一个表中存在匹配，则返回行
+
+SELECT u.unique_id, e.name
+FROM Employees e LEFT JOIN EmployeeUNI u
+ON e.id = u.id
